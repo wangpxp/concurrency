a6eb1af17872518656a61341d5eb8fb3aa42909a
wangpxp <wangpxp@gmail.com>
2020-01-05 20:30:05 +0800

    StampedLock

---
+++ src/main/java/com/wangpxp/concurrency/example/aqs/LockExample1.java
@@ -0,0 +1,52 @@
+package com.wangpxp.concurrency.example.aqs;
+
+import com.wangpxp.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@ThreadSafe
+public class LockExample1 {
+
+    //请求总数
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        // 定义线程池
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        // 定义信号量，信号量的数量和线程数相同
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        // 定义CountDownLatch,CountDownLatch可以阻塞线程，在一个特定条件满足后继续执行。
+        //
+        // CountDownLatch能够使一个线程在等待另外一些线程完成各自工作之后，再继续执行。使用一个计数器进行实现。
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire(); // 信号量执行
+                    add();
+                    semaphore.release(); // 信号量释放
+                } catch (InterruptedException e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown(); //countDownLatch计数器减一
+            });
+        }
+        countDownLatch.await(); // 计数器减到0， countDownLatch的await线程可以被唤醒，在此之前等待
+        log.info("count:{}", count);
+    }
+
+    private synchronized static void add() {
+        count++;
+    }
+}
---
+++ src/main/java/com/wangpxp/concurrency/example/aqs/LockExample3.java
@@ -0,0 +1,51 @@
+package com.wangpxp.concurrency.example.aqs;
+
+import com.wangpxp.concurrency.annotations.ThreadSafe;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+@Slf4j
+@ThreadSafe
+public class LockExample3 {
+
+    private final Map<String, Data> map = new TreeMap<>();
+
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
+    private final Lock readLock = lock.readLock();
+
+    private final Lock writeLock = lock.writeLock();
+
+    public Data get(String key) {
+        readLock.lock();
+        try {
+            return map.get(key);
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    public Set<String> getAllkeys() {
+        readLock.lock();
+        try {
+            return map.keySet();
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    public Data put(String key, Data value) {
+        writeLock.lock();
+        try {
+            return map.put(key, value);
+        } finally {
+            readLock.unlock();
+        }
+    }
+}
