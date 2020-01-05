ce7e56784d40a226259404f3ad3a5b32741c1725
wangpxp <wangpxp@gmail.com>
2020-01-05 17:02:53 +0800

    CyclicBarrier

---
+++ src/main/java/com/wangpxp/concurrency/example/aqs/CyclicBarrierExample2.java
@@ -0,0 +1,40 @@
+package com.wangpxp.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+@Slf4j
+public class CyclicBarrierExample2 {
+
+    private static CyclicBarrier barrier = new CyclicBarrier(5);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 10; i++) {
+            final int threadNum = i;
+            Thread.sleep(1000);
+            executor.execute(() -> {
+                try {
+                    race(threadNum);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        executor.shutdown();
+    }
+
+    private static void race(int threadNum) throws Exception {
+        Thread.sleep(1000);
+        log.info("{} is ready", threadNum);
+        try {
+            barrier.await(2000, TimeUnit.MILLISECONDS);
+        } catch (BrokenBarrierException | TimeoutException e) {
+            log.warn("BrokenBarrierException", e);
+        }
+        log.info("{} continue", threadNum);
+    }
+}
