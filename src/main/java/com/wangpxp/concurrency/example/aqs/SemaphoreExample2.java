4a2fa9e1a24ef215dd0fb4c0c4e07441db5fd7c0
wangpxp <wangpxp@gmail.com>
2020-01-05 16:47:58 +0800

    Semaphore

---
+++ src/main/java/com/wangpxp/concurrency/example/aqs/SemaphoreExample2.java
@@ -0,0 +1,40 @@
+package com.wangpxp.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+public class SemaphoreExample2 {
+    private final static int threadCount = 200;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService exec = Executors.newCachedThreadPool();
+
+        final Semaphore semaphore = new Semaphore(3); //同时允许3个线程执行
+
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNum = i;
+            exec.execute(()-> {
+                try {
+                    semaphore.acquire(3); // 获取3个许可
+                    test(threadNum);
+                    semaphore.release(3); // 释放3个许可
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                }
+            });
+        }
+        log.info("finish");
+        exec.shutdown();
+    }
+
+    private static void test(int threadNum) throws InterruptedException {
+        log.info("{}", threadNum);
+        Thread.sleep(1000);
+    }
+}
